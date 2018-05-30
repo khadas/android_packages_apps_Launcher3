ecfd09915cbe7b747b562b049f890928fa980c29
wh <wanghang@rock-chips.com>
2018-05-30 16:05:40 +0800

    Launcher3: hide documentsui icon with go
    Change-Id: I6c4baa28e8fa9c9c3ae63da24a9e0df7bcacffa0
    Signed-off-by: wh <wanghang@rock-chips.com>

--- src/com/android/launcher3/AppFilter.java
+++ src/com/android/launcher3/AppFilter.java
@@ -4,12 +4,23 @@ import android.content.ComponentName;
 import android.content.Context;
 
 public class AppFilter {
+    private static boolean mIsGoLauncher = false;
 
     public static AppFilter newInstance(Context context) {
+        String gmsProp = Utilities.getSystemProperty("ro.com.google.gmsversion", "");
+        mIsGoLauncher = null != gmsProp && gmsProp.contains(".go");
         return Utilities.getOverrideObject(AppFilter.class, context, R.string.app_filter_class);
     }
 
     public boolean shouldShowApp(ComponentName app) {
+        if (null != app && mIsGoLauncher){
+            String packageName = app.getPackageName();
+            String className = app.getClassName();
+            if("com.android.documentsui".equals(packageName)
+                && "com.android.documentsui.LauncherActivity".equals(className)){
+                return false;
+            }
+        }
         return true;
     }
 }
