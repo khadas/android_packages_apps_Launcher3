6a733073f6f57b505ac2b8cde23c634a28f4149f
Bian Jin chen <kenjc.bian@rock-chips.com>
2018-07-31 15:57:07 +0800

    Launcher3: default to hide Files while using GMS Express.
    Change-Id: I50f4c7208b1c51e3e6634f894f0d612d124021ac
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>

--- src/com/android/launcher3/AppFilter.java
+++ src/com/android/launcher3/AppFilter.java
@@ -4,16 +4,16 @@ import android.content.ComponentName;
 import android.content.Context;
 
 public class AppFilter {
-    private static boolean mIsGoLauncher = false;
+    private static boolean mShouldHideFiles = false;
 
     public static AppFilter newInstance(Context context) {
         String gmsProp = Utilities.getSystemProperty("ro.com.google.gmsversion", "");
-        mIsGoLauncher = null != gmsProp && gmsProp.contains(".go");
+        mShouldHideFiles = !android.text.TextUtils.isEmpty(gmsProp);
         return Utilities.getOverrideObject(AppFilter.class, context, R.string.app_filter_class);
     }
 
     public boolean shouldShowApp(ComponentName app) {
-        if (null != app && mIsGoLauncher){
+        if (null != app && mShouldHideFiles){
             String packageName = app.getPackageName();
             String className = app.getClassName();
             if("com.android.documentsui".equals(packageName)
