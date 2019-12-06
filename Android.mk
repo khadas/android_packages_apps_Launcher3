b27cfe7a3a65488e480edae5c7ced9ebd242e960
Bian Jin chen <kenjc.bian@rock-chips.com>
2019-12-06 15:14:46 +0800

    launcher3Go: enable widget for Android Go build.
    Change-Id: I89b91b1c1316de88c705ee1b268ee5e34a231eab
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>

--- Android.mk
+++ Android.mk
@@ -121,7 +121,8 @@ LOCAL_STATIC_ANDROID_LIBRARIES := Launcher3CommonDepsLib
 LOCAL_SRC_FILES := \
     $(call all-java-files-under, src) \
     $(call all-java-files-under, src_ui_overrides) \
-    $(call all-java-files-under, go/src)
+    $(call all-java-files-under, go/src) \
+    $(call all-java-files-under, src_shortcuts_overrides)
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/go/res
 
@@ -238,7 +239,8 @@ LOCAL_SRC_FILES := \
     $(call all-java-files-under, src) \
     $(call all-java-files-under, quickstep/src) \
     $(call all-java-files-under, quickstep/recents_ui_overrides/src) \
-    $(call all-java-files-under, go/src)
+    $(call all-java-files-under, go/src) \
+    $(call all-java-files-under, src_shortcuts_overrides)
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/quickstep/res \
@@ -285,7 +287,8 @@ LOCAL_SRC_FILES := \
     $(call all-java-files-under, src) \
     $(call all-java-files-under, quickstep/src) \
     $(call all-java-files-under, go/src) \
-    $(call all-java-files-under, go/quickstep/src)
+    $(call all-java-files-under, go/quickstep/src) \
+    $(call all-java-files-under, src_shortcuts_overrides)
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/quickstep/res \
