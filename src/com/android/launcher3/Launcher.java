4845ac57b183be99a80ba606c7bd30e31ed1c4e3
Terry <terry@khadas.com>
2020-05-27 18:41:23 +0800

    disable discovery bounce animation

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -788,7 +788,6 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
         // Refresh shortcuts if the permission changed.
         mModel.refreshShortcutsIfRequired();
 
-        DiscoveryBounce.showForHomeIfNeeded(this);
         if (mLauncherCallbacks != null) {
             mLauncherCallbacks.onResume();
         }
